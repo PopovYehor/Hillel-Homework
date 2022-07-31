a75964688cc669daf72550060025730439dd2305
PopovYehor <popov.yehor.it@gmail.com>
2022-07-31 16:45:20 +0300

    28

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,33 +1,48 @@
-
+import "./style.scss"
 import { createElem } from "../../helper/createElement"
 import { advertising } from "./advertising"
 import { promoution } from "./promoution/promoution-index"
-import { catalog } from "./catalog/catalog-index"
 import { special } from "./special/special-index"
 import { testDrive } from "./test-drive/test-drive-index"
 import { client } from "./client/client-index"
 import { review } from "./review/review-index"
-import { question } from "./question/question-index"
 
+import { catalogSelect } from "../../components/catalog/catalog-select"
+import { catalogItems } from "../../components/catalog/catalog-item"
+import { catalogTitle } from "../../components/catalog/catalog-title"
+import { viewAll } from "../../components/catalog/view-all"
+import { question } from "../../components/question"
+import { map } from "../../components/question/map"
 
 const Main = ()=>{
     const maine = document.getElementById('main')
     createElem('div', 'advertising', null, maine)
     advertising()
+
     createElem('div', 'promoution-wrap', null, maine)
     promoution()
-    createElem('div', 'catalog-wrap', null, maine)
-    catalog()
+
+    const catalogWrap = createElem('div', 'catalog-wrap', null, maine)
+    catalogTitle('Каталог кресел', catalogWrap, '-main')
+    catalogSelect()
+    catalogItems()
+    viewAll()
+
     createElem('div', 'specials-wrap', null, maine)
     special()
+
     createElem('div', 'test-wrap', null, maine)
     testDrive()
+
     createElem('div', 'client-wrap', null, maine)
     client()
+
     createElem('div', 'reviews-wrap', null, maine)
     review()
+
     createElem('div', 'question-wrap', null, maine)
     question()
+    map('main')
 }
 
 export default Main
